a08caf647023bdfd8e83c93d2874e07b2a753f23
AndrzejWojciechowski0 <wojciechowski969@gmail.com>
2019-03-21 19:29:36 +0100

    update

--- calc.py
+++ calc.py
@@ -1,1 +1,42 @@
+def calc_add(a, b):
+    print('{} + {} ='.format(str(a), str(b)), end=' ')
+    return a + b
 
+def calc_subtract(a, b):
+    print('{} - {} ='.format(str(a), str(b)), end=' ')
+    return a - b
+
+def calc_multiple(a, b):
+    print('{} * {} ='.format(str(a), str(b)), end=' ')
+    return a * b
+
+def calc_divide(a, b):
+    print('{} / {} ='.format(str(a), str(b)), end=' ')
+    return a / b
+    if 
+
+def check_operation_name(operation):
+    
+    if operation in method_dict:
+        return method_dict[operation]
+    operation = input('Podaj dzialanie: dodawanie, odejmowanie, mnozenie, dzielenie : ')
+    check_operation_name(operation)
+            
+
+method_dict = {
+    'dodawanie': calc_add,
+    'odejmowanie': calc_subtract,
+    'mnozenie': calc_multiple,
+    'dzielenie': calc_divide,
+}
+
+if __name__ == "__main__":
+    print('Witam w moim kalkulatorze :)')
+    while True
+        operation = input('Podaj dzialanie: dodawanie, odejmowanie, mnozenie, dzielenie : ')
+        if operation == 'exit':
+            break
+        operation_def = check_operation_name(operation)
+        a = input('Podaj pierwsza liczbe: ')
+        b = input('Podaj druga liczbe: ')
+        print(operation_def(int(a), int(b)))
